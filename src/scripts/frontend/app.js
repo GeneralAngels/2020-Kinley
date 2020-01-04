66ddc6b1a6ac5af2da2ff984a8003bf6ee6fdc40
Nadav Tasger <tashernadav@gmail.com>
2020-01-04 21:18:59 +0200

    Slow progress

--- src/scripts/frontend/app.js
+++ src/scripts/frontend/app.js
@@ -1,4 +1,4 @@
 function load() {
-    
+    page("home");
 }
 
---
+++ src/scripts/frontend/kinley/api.js
@@ -0,0 +1,27 @@
+const FieldHeight = 0;
+const FieldWidth = 0;
+
+let path = [];
+
+let ScreenXtoFieldX = 1;
+let ScreenYtoFieldY = 1;
+
+
+function define_size_of_canvas() {
+
+}
+
+// Path functions
+
+function path_create() {
+    path = [];
+}
+
+function path_add_point(screenX, screenY) {
+
+}
+
+function path_compile(){
+    let compiledPath = [];
+    for ()
+}
